2534902d7f123b7d149c6dcc6b26b80d3cdef93b
schmidward <eric.d.schmid1@gmail.com>
2023-03-17 16:21:51 -0500

    controllers properly hooked up to the persisting database. All tasktwotests passed.

--- queries.sql
+++ queries.sql
@@ -1,6 +1,15 @@
 -- Part 1: Test it with SQL
+-- found this solution here: https://datatofish.com/data-type-columns-sql-server/
+--    SELECT
+--    TABLE_NAME,
+--    COLUMN_NAME,
+--    DATA_TYPE
+--    FROM INFORMATION_SCHEMA.COLUMNS
+--    WHERE TABLE_NAME = 'job';
 
 -- Part 2: Test it with SQL
+    SELECT name FROM employer
+    WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
